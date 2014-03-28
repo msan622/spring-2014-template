2caa424365e15a8926627a4062767d1c852615b9
Rachel Smith <rsmith902@gmail.com>
2014-03-27 21:07:12 -0700

    ..

--- homework1/homework_1.R
+++ homework1/homework_1.R
@@ -20,37 +20,50 @@ genre[which(count == 1 & movies$Documentary == 1)] = "Documentary"
 genre[which(count == 1 & movies$Romance == 1)] = "Romance"
 genre[which(count == 1 & movies$Short == 1)] = "Short"
 movies$genre = genre
-movies$trans_budget <- movies$budget/1000000
 # remove movies with budget of zero or less
 movies_subset <- subset(movies, movies$budget > 0)
 
 # custom palette for genres
 my_palette <- c("sienna2", "steelblue3", "darkolivegreen3", "slateblue3", "#333366", "bisque3","slategray4", 'brown3', "gold3")
 
+million_formatter <- function(x) {
+  #label <- round(x / 1000)
+  return(sprintf("$%dM", x / 1000000))
+}
+
 # first plot
-scatter_plot <- ggplot(movies_subset, aes(x=trans_budget, y=rating)) + 
-  geom_point(color = 'slategray4') +  
-  xlab('Budget (in millions)') + ylab('IMDb User Rating') + 
-  ggtitle('IMDb User Rating Vs. Budget') + 
-  scale_x_continuous(labels=dollar) + theme(text = element_text(size=10))
+scatter_plot <- ggplot(movies_subset, aes(x=budget, y=rating)) + 
+  geom_point(color = 'slategray4', alpha = .8) +  
+  xlab('Budget') + ylab('User Rating') + 
+  ggtitle('User Rating Vs. Budget') + 
+  scale_x_continuous(label=million_formatter) + 
+  scale_y_continuous(breaks=c(2,6,10))+
+  theme(text = element_text(size=10))
 print(scatter_plot)
 
 # second plot
 bar_plot <- ggplot(movies_subset, aes(x = genre, y = ..count..)) + 
   geom_bar(aes(fill = factor(genre))) +  
-  xlab('Genre') + ylab('Count') + 
   ggtitle('Count of Movies by Genre') + 
   guides(fill=FALSE) + 
-  scale_fill_manual(values=my_palette) 
+  scale_y_continuous(expand = c(0, 25))+
+  scale_fill_manual(values=my_palette) + 
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        panel.grid.major.x = element_blank(), 
+        panel.grid.minor.y = element_blank(), 
+        axis.ticks.x = element_blank(), 
+        axis.text.x = element_text(size = 12))
 print(bar_plot)
 
 # third plot
-small_mult_plot <- ggplot(movies_subset, aes(x=trans_budget, y=rating, group = factor(genre))) + 
+small_mult_plot <- ggplot(movies_subset, aes(x=budget, y=rating, group = factor(genre))) + 
   geom_point(aes(colour = factor(genre))) +  
-  xlab('Budget (in millions)') + ylab('IMDb User Rating') + 
+  xlab('Budget') + ylab('User Rating') + 
   ggtitle('IMDb User Rating Vs. Budget by Genre') + 
   facet_wrap(~genre, ncol = 3)  + 
-  scale_x_continuous(labels = dollar) + 
+  scale_x_continuous(label=million_formatter) + 
+  scale_y_continuous(breaks=c(2,6,10)) + 
   guides(colour=FALSE) + scale_colour_manual(values=my_palette) 
 print(small_mult_plot)
 
@@ -60,13 +73,12 @@ eu <- transform(data.frame(EuStockMarkets), time = as.numeric(time(EuStockMarket
 eu_m <- melt(eu, id.vars = 'time', value = c('DAX', 'SMI', 'CAC', 'FTSE'))
 
 # pallete
-eu_palette <- c('#1f78b4', '#e31a1c', '#33a02c', '#fb8072')
 
 new_lineplot <- ggplot(eu_m, aes(x=time, y=value, group = as.factor(variable), color = as.factor(variable))) + 
   geom_line() + xlab('Time') +
   ggtitle('European Stock Indexes') +  
   ylab('Value') + theme(text = element_text(size=10)) + 
-  scale_colour_manual('Index', values= eu_palette)
+  scale_colour_brewer(type = 'qual', palette = 7, name = 'Index')
 print(new_lineplot)
 
 # saving files
